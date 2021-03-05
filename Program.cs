a124a58e2317400ee786be0b4c7eeb54a452e39b
LunarDiver <25Prozent@gmail.com>
2021-03-05 11:41:06 +0100

    Added url/port for application.

--- Program.cs
+++ Program.cs
@@ -21,6 +21,7 @@ namespace SmolyShortener
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseUrls("http://*:80");
                 });
     }
 }
