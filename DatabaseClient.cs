8526247a76f8e51d6c3741137b49eb41d7760e5c
LunarDiver <25Prozent@gmail.com>
2021-03-05 18:00:46 +0100

    Added basic wrapper methods.

--- DatabaseClient.cs
+++ DatabaseClient.cs
@@ -1,7 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
 using System;
 using System.IO;
 using System.Data.SQLite;
 using System.Threading.Tasks;
+using System.Linq;
 
 namespace SmolyShortener
 {
@@ -21,6 +24,58 @@ namespace SmolyShortener
             }.ToString();
             _connection = new SQLiteConnection(connection);
             _connection.Open();
+        }
+
+        public async Task CreateTable(string table, params string[] columns)
+        {
+            var command = new SQLiteCommand(
+                $"create table {table}({string.Join(',', columns)})",
+                _connection);
+
+            await command.ExecuteNonQueryAsync();
+        }
+
+        public async Task<int> Write(string table, params (string column, string value)[] insertInfo)
+        {
+            for (int i = 0; i < insertInfo.Length; i++)
+                insertInfo[i].value = string.Join(string.Empty, insertInfo[i].value.Prepend('\'').Append('\''));
+
+            string columns = string.Join(',', insertInfo.Select(info => info.column));
+            string values = string.Join(',', insertInfo.Select(info => info.value));
+
+            var command = new SQLiteCommand(
+                $"insert into {table}({columns}) " +
+                $"values({values})",
+                _connection);
+
+            return await command.ExecuteNonQueryAsync();
+        }
+
+        public async IAsyncEnumerable<object[]> Read(
+            string table,
+            IEnumerable<string> columns,
+            int maxRows,
+            IEnumerable<string> conditions = null,
+            bool unique = false)
+        {
+            string colsString = string.Join(',', columns);
+            string condsString = conditions != null
+                ? $"where {string.Join(" and ", conditions)} "
+                : "";
+
+            var command = new SQLiteCommand(
+                $"select {(unique ? "distinct " : "")}{colsString} from {table} " +
+                condsString +
+                $"limit {maxRows}",
+                _connection);
+
+            using var reader = await command.ExecuteReaderAsync();
+            while (await reader.ReadAsync())
+            {
+                var row = new object[reader.FieldCount];
+                reader.GetValues(row);
+                yield return row;
+            }
         }
 
         /// <summary>
