3a8293c5ba0cdc477d9d33fa6fabca9bd26d8163
LunarDiver <25Prozent@gmail.com>
2021-03-09 09:51:11 +0100

    Removed commented test code.

--- Controllers/ShorteningController.cs
+++ Controllers/ShorteningController.cs
@@ -7,19 +7,6 @@ namespace SmolyShortener.Controllers
     [ApiController]
     public class ShorteningController : ControllerBase
     {
-        // var client = new DatabaseClient("test.db");
-        // await client.CreateTable("test", "testcol1 INT PRIMARY KEY NOT NULL", "testcol2 TEXT");
-        // await client.Write("test", new[]
-        // {
-        //     "'testcol1'",
-        //     "'testcol2'"
-        // }, new[]
-        // {
-        //     69.ToString(),
-        //     "testvalue"
-        // });
-        // client.Dispose();
-
         [HttpGet("{id}")]
         public async Task<IActionResult> GetHttp(string id)
         {
